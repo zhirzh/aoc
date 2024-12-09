1983f4ca1e49377933d7eba0a27870b3146d5f7b
Shirsh Zibbu <zibbu.shirsh@gmail.com>
2024-12-09 11:54:01 +0530

    9.1

---
+++ 9/solve.ts
@@ -0,0 +1,38 @@
+import { readFileSync } from "fs";
+
+const nums = readFileSync(
+  import.meta.resolve("./sample").replace("file://", ""),
+  "utf8"
+)
+  .split("")
+  .map(Number);
+
+const blocks: number[] = [];
+const holeIdxs: number[] = [];
+
+let diskIdx = 0;
+nums.forEach((num, i) => {
+  if (i % 2 === 0) {
+    const id = i / 2;
+    blocks.push(...Array(num).fill(id));
+    diskIdx += num;
+  } else {
+    for (let j = 0; j < num; j++) {
+      holeIdxs.push(diskIdx);
+      diskIdx++;
+    }
+  }
+});
+
+while (holeIdxs.length > 0) {
+  let holeIdx = holeIdxs.shift()!;
+  let block = blocks.pop()!;
+  blocks.splice(holeIdx, 0, block);
+}
+
+let p1 = 0;
+blocks.forEach((num, i) => {
+  p1 += num * i;
+});
+
+console.log(p1 === 6225730762521);
