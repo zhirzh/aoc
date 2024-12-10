d83a55135013480ef5a0c0de50ccb0a1375a1008
Shirsh Zibbu <zibbu.shirsh@gmail.com>
2024-12-10 12:48:42 +0530

    10

---
+++ 10/solve.ts
@@ -0,0 +1,69 @@
+import { readFileSync } from "fs";
+import { inRange, printGrid, readGrid } from "../utils";
+
+const grid = readGrid("./10/input");
+const R = grid.length;
+const C = grid[0].length;
+
+type Point = [r: number, c: number];
+type Trail = Point[];
+
+let trails: Trail[] = [];
+grid.forEach((row, r) => {
+  row.forEach((x, c) => {
+    if (x === 0) {
+      trails.push([[r, c]]);
+    }
+  });
+});
+
+for (let i = 1; i < 10; i++) {
+  const nextTrails: Trail[] = [];
+
+  trails.forEach((trail) => {
+    const last = trail[trail.length - 1];
+    const [r, c] = last;
+
+    const around = [
+      [r - 1, c],
+      [r, c - 1],
+      [r, c + 1],
+      [r + 1, c],
+    ];
+
+    around.forEach(([ar, ac]) => {
+      if (!inRange(ar, 0, R) || !inRange(ac, 0, C)) return;
+      const y = grid[ar][ac];
+      if (y === i) {
+        nextTrails.push([...trail, [ar, ac]]);
+      }
+    });
+  });
+
+  trails = nextTrails;
+}
+
+const uniqueTrails: [first: Point, last: Point][] = [];
+
+trails.forEach((trail) => {
+  const first = trail[0];
+  const last = trail[trail.length - 1];
+  const exists = uniqueTrails.some((other) => {
+    const [otherFirst, otherLast] = other;
+    return (
+      first[0] === otherFirst[0] &&
+      first[1] === otherFirst[1] &&
+      last[0] === otherLast[0] &&
+      last[1] === otherLast[1]
+    );
+  });
+  if (!exists) {
+    uniqueTrails.push([first, last]);
+  }
+});
+
+const p1 = uniqueTrails.length;
+const p2 = trails.length;
+
+console.log(p1 === 776);
+console.log(p2 === 1657);
--- utils.ts
+++ utils.ts
@@ -1,3 +1,5 @@
+import { readFileSync } from "fs";
+
 export function zip<T>(a: T[], b: T[]): [T, T][] {
   if (a.length !== b.length) throw new Error("zip: length mismatch");
   return a.map((x, i) => [x, b[i]]);
@@ -11,6 +13,14 @@ export function inRange(x: number, lo: number, hi: number) {
   }
 
   return lo <= x && x < hi;
+}
+
+export function readGrid(file: string) {
+  const { pathname } = new URL(file, import.meta.url);
+  const text = readFileSync(pathname, "utf8");
+  const lines = text.split("\n");
+  const grid = lines.map((line) => line.split(""));
+  return grid.map((row) => row.map(Number));
 }
 
 export function printGrid(grid: unknown[][]) {
