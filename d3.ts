e346d8e9141ccc25839b913ba34907cb5f84e622
Shirsh Zibbu <zibbu.shirsh@gmail.com>
2026-01-01 23:20:41 +0530

    day 3

---
+++ d3.ts
@@ -0,0 +1,43 @@
+import { readFileSync } from "node:fs";
+import { Result, test } from "./utils";
+
+const sample = `987654321111111
+811111111111119
+234234234234278
+818181911112111`;
+
+const input = readFileSync("./d3.txt", "utf8");
+
+test(solve(sample), [357, null]);
+test(solve(input), [17095, 43287141963]);
+
+function solve(input: string): Result {
+  let ans1 = 0;
+  let ans2 = 0;
+
+  input.split("\n").forEach((bank) => {
+    const nums = bank.split("").map(Number);
+    const k1 = maxIndex(nums);
+    const k2 =
+      k1 === nums.length - 1
+        ? maxIndex(nums.slice(0, k1))
+        : k1 + 1 + maxIndex(nums.slice(k1 + 1));
+
+    const jolt = k1 < k2 ? 10 * nums[k1] + nums[k2] : 10 * nums[k2] + nums[k1];
+    console.log(jolt);
+    ans1 += jolt;
+  });
+
+  return [ans1, ans2];
+}
+
+function maxIndex(nums: number[]) {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const x = nums[i];
+    const max = nums[k];
+    if (x > max) k = i;
+  }
+
+  return k;
+}
